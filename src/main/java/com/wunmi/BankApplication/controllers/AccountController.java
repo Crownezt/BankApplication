aac0c9eccafcfafb8d0fc6fb2afcf1c5cd377283
Adewunmi <adepheranmie@gmail.com>
2023-01-03 20:26:36 +0100

    bankApp

--- src/main/java/com/wunmi/BankApplication/controllers/AccountController.java
+++ src/main/java/com/wunmi/BankApplication/controllers/AccountController.java
@@ -7,10 +7,7 @@ import com.wunmi.BankApplication.services.AccountServices;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -27,6 +24,8 @@ public class AccountController {
         catch(EmailAlreadyExistException e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+
+        public ResponseEntity<?> deposit()
     }
 
 
